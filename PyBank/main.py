b0048aa5d8b61d0f4e51820c36cc8e72b841001f
taliehshmail-GgmnNu <talieh.sh.mail@gmail.com>
2023-09-19 17:23:04 +1000

    Solution1-PyBank

---
+++ PyBank/main.py
@@ -0,0 +1,70 @@
+#PyBank 
+# Importing Modules
+import os
+import csv
+pybank_csv=os.path.join("Resources","budget_data.csv")
+#Open and read csv
+with open(pybank_csv) as csv_file:
+    csv_reader=csv.reader(csv_file, delimiter=",")
+    next(csv_reader)
+
+    
+
+    # Calculating the total number of months and Total included in the dataset
+    Total_Months=0
+    Total=0
+    previous_amount=0
+    greatest_increase_amount=0
+    greatest_increase_month=[]
+    greatest_decrease_amount=0
+    greatest_decrease_month=[]
+    for row in csv_reader:
+        if row[0]!="":
+            if int(row[1])-previous_amount>greatest_increase_amount:
+                greatest_increase_amount=int(row[1])-previous_amount
+                previous_amount=int(row[1])
+                greatest_increase_month=str(row[0])
+            if int(row[1])-previous_amount<greatest_decrease_amount:
+                greatest_decrease_amount=int(row[1])-previous_amount
+                previous_amount=int(row[1])
+                greatest_decrease_month=str(row[0])
+            previous_amount=int(row[1])
+            Total_Months+=1
+            Total+=int(row[1])
+#    print("Total Month: "+ str(Total_Months))
+#    print("Total : $"+ str(Total))
+#    print("greates increase amount :"+str(greatest_increase_amount))
+#    print("greates increase month :"+str(greatest_increase_month))
+#    print("greates decrease amount :"+str(greatest_decrease_amount))
+#    print("greates decrease month :"+str(greatest_decrease_month))
+#Calculating Average Change 
+with open(pybank_csv) as csv_file:
+    csv_reader=csv.reader(csv_file, delimiter=",")
+    next(csv_reader)    
+    #Converting the csv_reader to a list of lists to get access to the first and last row by indexing
+    csv_list=list(csv_reader)
+    last_month=len(csv_list)-1
+    first_month_profit=int(csv_list[0][1])
+    last_month_profit=int(csv_list[last_month][1])
+    if Total_Months>0:
+        average_change=round((last_month_profit-first_month_profit)/(Total_Months-1),2)
+
+    else:
+        average_change=0
+
+
+
+
+    print("Total Month: "+ str(Total_Months))
+    print("Total : $"+ str(Total))
+    print("average change: ",str(average_change))
+    print("greates increase amount :"+str(greatest_increase_amount))
+    print("greates increase month :"+str(greatest_increase_month))
+    print("greates decrease amount :"+str(greatest_decrease_amount))
+    print("greates decrease month :"+str(greatest_decrease_month))
+
+
+
+
+
+
