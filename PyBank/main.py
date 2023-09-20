698902b266ac33422493aa31a2d84c53caf1fc54
taliehshmail-GgmnNu <talieh.sh.mail@gmail.com>
2023-09-20 14:58:12 +1000

    Adding comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,15 +2,17 @@
 # Importing Modules
 import os
 import csv
+
+#Csv path to get csv
 pybank_csv=os.path.join("Resources","budget_data.csv")
+
 #Open and read csv
 with open(pybank_csv) as csv_file:
     csv_reader=csv.reader(csv_file, delimiter=",")
     next(csv_reader)
 
-    
-
     # Calculating the total number of months and Total included in the dataset
+    #Initial variables
     Total_Months=0
     Total=0
     previous_amount=0
@@ -18,8 +20,11 @@ with open(pybank_csv) as csv_file:
     greatest_increase_month=[]
     greatest_decrease_amount=0
     greatest_decrease_month=[]
+    
+    #Iterate over each row in csv
     for row in csv_reader:
         if row[0]!="":
+            #Calculating greatest inctease and decrease
             if int(row[1])-previous_amount>greatest_increase_amount:
                 greatest_increase_amount=int(row[1])-previous_amount
                 previous_amount=int(row[1])
@@ -31,6 +36,7 @@ with open(pybank_csv) as csv_file:
             previous_amount=int(row[1])
             Total_Months+=1
             Total+=int(row[1])
+
 #Calculating Average Change 
 with open(pybank_csv) as csv_file:
     csv_reader=csv.reader(csv_file, delimiter=",")
@@ -48,14 +54,15 @@ with open(pybank_csv) as csv_file:
 
 
 
-
+    #Printing the results
+    print("Financial Analysis")
+    print("----------------------------")
     print("Total Month: "+ str(Total_Months))
     print("Total : $"+ str(Total))
-    print("average change: ",str(average_change))
-    print("greates increase amount :"+str(greatest_increase_amount))
-    print("greates increase month :"+str(greatest_increase_month))
-    print("greates decrease amount :"+str(greatest_decrease_amount))
-    print("greates decrease month :"+str(greatest_decrease_month))
+    print("Average change: ",str(average_change))
+    print("Greates increase in profit :"+str(greatest_increase_month)+"($"+str(greatest_increase_amount)+")")
+    print("Greates increase in profit :"+str(greatest_decrease_month)+"($"+str(greatest_decrease_amount)+")")
+
 
 
 
